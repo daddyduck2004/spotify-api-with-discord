c3646941a1893ee73699fb170ba006206da3fcdc
daddyduck2004 <74186916+daddyduck2004@users.noreply.github.com>
2020-11-19 17:38:30 +0700

    Update getCurrentSongInfo.py

--- examples/getCurrentSongInfo.py
+++ examples/getCurrentSongInfo.py
@@ -4,5 +4,5 @@ client = Spotify("discord_token") #discord token connected to spotify.
 
 player = client.getCurrentPlaying().json()
 songName = player["item"]["name"]
-songDuration = player["item"]["duration_ms"] / 1000 #return in ms but multiply 1000 to get second.
-songProgress = player["progress_ms"] / 1000 #return in ms but multiply 1000 to get second.
+songDuration = player["item"]["duration_ms"] / 1000 #return in ms and convert to second.
+songProgress = player["progress_ms"] / 1000 #return in ms and convert to second.
